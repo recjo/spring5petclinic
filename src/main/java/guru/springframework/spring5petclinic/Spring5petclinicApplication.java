c3cfbdf114967d57f739af0ec5c255bbc29844cc
Joel Rec. bol <jrecinos@brandedonline.com>
2020-09-30 17:47:11 -0700

    initial upload

---
+++ src/main/java/guru/springframework/spring5petclinic/Spring5petclinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5petclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5petclinicApplication.class, args);
+    }
+
+}
