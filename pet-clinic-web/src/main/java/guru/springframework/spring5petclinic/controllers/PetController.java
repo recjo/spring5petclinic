d8e73f50455be0546a492f4dbe861dd9f5d2591f
Joel Rec. bol <jrecinos@brandedonline.com>
2020-10-12 17:45:01 -0700

    updated Pets/Vets controller to pass data

--- pet-clinic-web/src/main/java/guru/springframework/spring5petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/spring5petclinic/controllers/PetController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5petclinic.controllers;
 
 import guru.springframework.spring5petclinic.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,7 +15,10 @@ public class PetController {
 
     //when request matches pet or pet.html,
     @RequestMapping({"/pet", "/pet.html", "/pets", "/pets.html"})
-    public String listPets() {
+    public String listPets(Model model) {
+        //first param is the name of the attribute (to fetch it by later)
+        model.addAttribute("pets", petService.findAll());
+
         return "pets"; //this is thymeleaf filename in templates folder
     }
 }
--- pet-clinic-web/src/main/java/guru/springframework/spring5petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/spring5petclinic/controllers/VetController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5petclinic.controllers;
 
 import guru.springframework.spring5petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,7 +15,10 @@ public class VetController {
 
     //when request matches vet or vet.html,
     @RequestMapping({"/vet", "/vet.html", "/vets", "/vets.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        //first param is the name of the attribute (to fetch it by later)
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets"; //this is thymeleaf filename in templates folder
     }
 }
